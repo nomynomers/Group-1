24089aed7b1c47f6359220cdd4626b6353ad4e7d
NgocLong216 <102666480+NgocLong216@users.noreply.github.com>
2025-06-23 21:39:18 +0700

    delete note

--- server/auth-system/src/main/java/com/example/auth_system/entity/User.java
+++ server/auth-system/src/main/java/com/example/auth_system/entity/User.java
@@ -16,7 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id") //  matches DB
+    @Column(name = "user_id")
     private int userId;
 
     @Column(name = "email", nullable = false, unique = true)
