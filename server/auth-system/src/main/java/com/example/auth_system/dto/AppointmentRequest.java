e0cf015f8649e4eeace2fe908146b8146de65cfa
tuanminh1407 <trantuanminh9324@gmail.com>
2025-06-29 19:45:00 +0700

    Update Appointment Request

--- server/auth-system/src/main/java/com/example/auth_system/dto/AppointmentRequest.java
+++ server/auth-system/src/main/java/com/example/auth_system/dto/AppointmentRequest.java
@@ -14,17 +14,11 @@ public class AppointmentRequest {
     @NotNull(message = "User ID is required")
     private Integer userID;
 
-    @NotNull(message = "Consultant ID is required")
-    private Integer consultantID;
-
     @NotNull(message = "Appointment date is required")
-    @Future(message = "Appointment date must be in the future")
     private LocalDate appointmentDate;
 
     @NotNull(message = "Start time is required")
     private LocalTime startTime;
 
-    @NotNull(message = "End time is required")
-    private LocalTime endTime;
 
 }
